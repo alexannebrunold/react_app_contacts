ae03e3dfe577dfe70e605311c474c848f1377b82
Alexanne Brunold <alexanne.brunold@hetic.net>
2021-02-14 11:56:42 +0100

    Show contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
 
 export default class Contact extends Component {
+  state = {
+    isContactShown: true,
+  }
+
+  showContact = () => {
+    this.setState({
+      isContactShown: !this.state.isContactShown,
+    })
+  }
+
   render() {
     return (
       <div className='m-4 p-2 align-center border-solid border-2 border-blue-400 '>
-        <h1 className='text-center font-medium'>{this.props.nom}</h1>
-        <div>
-          <p>Email: {this.props.email}</p>
-          <p>Téléphone: {this.props.tel}</p>
-        </div>
+        <h1 className='text-center font-medium'>
+          {this.props.nom}
+          <i className='fas fa-sort-down ml-2 cursor-pointer' onClick={this.showContact}></i>
+        </h1>
+        {this.state.isContactShown ? (
+          <div>
+            <p>Email: {this.props.email}</p>
+            <p>Téléphone: {this.props.tel}</p>
+          </div>
+        ) : null}
       </div>
     )
   }
